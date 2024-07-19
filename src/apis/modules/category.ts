4ba6d08c1f9f2fbb02e6c33853b8a6df2927b4eb
Vualidon1402 <nguyendanhkien1402@gmai.com>
2024-07-19 14:04:26 -0700

    update CategoryManager

--- src/apis/modules/category.ts
+++ src/apis/modules/category.ts
@@ -2,6 +2,6 @@ import axios from "axios"
 
 export const category = {
     async findAll() {
-        return await axios.get(`${import.meta.env.VITE_SV}/category`)
+        return await axios.get(`${import.meta.env.VITE_SV}/category/findAll`)
     }
 }
