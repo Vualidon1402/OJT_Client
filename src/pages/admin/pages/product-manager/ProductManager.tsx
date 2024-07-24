dca126a9fa045fc32ff820e8f13d0d767fcbb1cf
Vualidon1402 <nguyendanhkien1402@gmai.com>
2024-07-24 12:08:55 -0700

    Completed ui-api-integration-delete-productManager

--- src/pages/admin/pages/product-manager/ProductManager.tsx
+++ src/pages/admin/pages/product-manager/ProductManager.tsx
@@ -25,7 +25,7 @@ function ProductManager() {
   }, []);
 
   function handleEditProduct(productId: any) {
-    
+
     setCurrentProductId(productId);
     setEditFormState(true);
   }
@@ -44,90 +44,105 @@ function ProductManager() {
     setProducts((prevProducts) => [newProduct, ...prevProducts]);
   };
 
+  const handleDeleteProduct = (productId: number) => {
+    api.product.deleteProduct(productId)
+      .then(() => {
+        setProducts((prevProducts) =>
+          prevProducts.map(product =>
+            product.id === productId ? { ...product, status: false } : product
+          )
+        );
+      })
+      .catch(err => {
+        console.error("Delete error:", err);
+      });
+  };
   return (
     <div className='product_manager_box'>
-  {
-    editFormState ? (
-      <EditProduct productId={currentProductId} setEditFormState={setEditFormState} updateListProduct={updateListProduct} />
-    ) : (
-      addFormState ? (
-        <AddProduct setAddFormState={setAddFormState} updateListProduct={updateListProduct} />
-      ) : (
-        <>
-          <button onClick={() => setAddFormState(true)} className='btn btn-primary'>Create New</button>
-          <h1>Product Manager</h1>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Name</th>
-                <th>Image</th>
-                <th>Images</th>
-                <th>CreatedAt</th>
-                <th>UpdatedAt</th>
-                <th>Brand</th>
-                <th>Category</th>
-                <th>Description</th>
-                <th>Sku</th>
-                <th>Status</th>
-                <th>Comments</th>
-                <th>ProductDetail</th>
-                <th colSpan={2}>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                products.map((product, index) => (
-                  <tr key={product.id}>
-                    <td>{index + 1}</td>
-                    <td>{product?.productName}</td>
-                    <td><img src={product?.image} alt={product?.productName} width="50" height="50" /></td>
-                    <td>
-                      <button className='btn-show-image' onClick={() => handleShowImages(product?.images)}>
-                        Show Images
-                      </button>
-                    </td>
-                    <td>{product?.createdAt}</td>
-                    <td>{product?.updatedAt}</td>
-                    <td>{product?.brand?.brandName}</td>
-                    <td>{product?.category?.categoryName}</td>
-                    <td>{product?.description}</td>
-                    <td>{product?.sku}</td>
-                    <td>{product?.status ? "đang bán" : "ngừng bán"}</td>
-                    <td>rẻ đẹp</td>
-                    <td><a href="/">xem chi tiết</a></td>
-                    <td>
-                      <button className='btn btn-warning' onClick={() => handleEditProduct(product.id)}>Edit</button>
-                      <button className='btn btn-danger'>Delete</button>
-                    </td>
+      {
+        editFormState ? (
+          <EditProduct productId={currentProductId} setEditFormState={setEditFormState} updateListProduct={updateListProduct} />
+        ) : (
+          addFormState ? (
+            <AddProduct setAddFormState={setAddFormState} updateListProduct={updateListProduct} />
+          ) : (
+            <>
+              <button onClick={() => setAddFormState(true)} className='btn btn-primary'>Create New</button>
+              <h1>Product Manager</h1>
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Image</th>
+                    <th>Images</th>
+                    <th>CreatedAt</th>
+                    <th>UpdatedAt</th>
+                    <th>Brand</th>
+                    <th>Category</th>
+                    <th>Description</th>
+                    <th>Sku</th>
+                    <th>Status</th>
+                    <th>Comments</th>
+                    <th>ProductDetail</th>
+                    <th colSpan={2}>Action</th>
                   </tr>
-                ))
-              }
-            </tbody>
-          </Table>
+                </thead>
+                <tbody>
+                  {
+                    products.map((product, index) => (
+                      <tr key={product.id}>
+                        <td>{index + 1}</td>
+                        <td>{product?.productName}</td>
+                        <td><img src={product?.image} alt={product?.productName} width="50" height="50" /></td>
+                        <td>
+                          <button className='btn-show-image' onClick={() => handleShowImages(product?.images)}>
+                            Show Images
+                          </button>
+                        </td>
+                        <td>{product?.createdAt}</td>
+                        <td>{product?.updatedAt}</td>
+                        <td>{product?.brand?.brandName}</td>
+                        <td>{product?.category?.categoryName}</td>
+                        <td>{product?.description}</td>
+                        <td>{product?.sku}</td>
+                        <td>{product?.status ? "đang bán" : "ngừng bán"}</td>
+                        <td>rẻ đẹp</td>
+                        <td><a href="/">xem chi tiết</a></td>
+                        <td>
+                          <button className='btn btn-warning' onClick={() => handleEditProduct(product.id)}>Edit</button>
+                          {product.status && (
+                            <button className='btn btn-danger' onClick={() => handleDeleteProduct(product.id)}>Delete</button>
+                          )}
+                        </td>
+                      </tr>
+                    ))
+                  }
+                </tbody>
+              </Table>
 
-          <Modal show={showModal} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Product Images</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              {
-                currentImages.map(img => (
-                  <img key={img.id} src={img.src} alt="product" width="70px" height="50px" style={{ margin: '10px 10px 10px 10px' }} />
-                ))
-              }
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>
-                Close
-              </Button>
-            </Modal.Footer>
-          </Modal>
-        </>
-      )
-    )
-  }
-</div>
+              <Modal show={showModal} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Product Images</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  {
+                    currentImages.map(img => (
+                      <img key={img.id} src={img.src} alt="product" width="70px" height="50px" style={{ margin: '10px 10px 10px 10px' }} />
+                    ))
+                  }
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Close
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            </>
+          )
+        )
+      }
+    </div>
 
   );
 }
