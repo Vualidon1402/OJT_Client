1ef01e5addb4fb627115bad7531fd5bbf506b858
Vualidon1402 <nguyendanhkien1402@gmai.com>
2024-07-26 11:47:50 -0700

    Completed ui-logout-Manager

--- src/pages/admin/components/Navbar.tsx
+++ src/pages/admin/components/Navbar.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
+  const userStore = useSelector((state: any) => state.userStore);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+  }
   return (
     <div id="navbar_box">
       <div className="user_box">
         <Link to={"/manager"}> HOME </Link>
-        {/* <img src={userStore.data?.avatarUrl} alt="avatar" /> */}
-        {/* <div className="user_name">{userStore.data?.userName}</div> */}
       </div>
-      <div className="authen_box"></div>
+      <div className="authen_box">
+        <img src={userStore.data?.avatarUrl} alt="avatar" />
+        <div className="user_name">{userStore.data?.userName}</div>
+        <button className='btn btn-danger' onClick={handleLogout}>LogOut</button>
+      </div>
     </div>
   );
 }
